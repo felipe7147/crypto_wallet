65a398148c540ba319efd500ca998aa0ff4cddff
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2021-07-29 20:52:10 +0000

    Refatorando o seeds.rb(find_or_create_by)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,37 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Cadastrando Moedas..."
+spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas...")
+spinner.auto_spin 
 
-Coin.create!(
-	description: "Bitcoin",
-	acronym: "BTC",
-	url_image: "https://image.flaticon.com/icons/png/512/3342/3342766.png"
-)
+coins = [
+	  	 {
+		  description: "Bitcoin",
+		  acronym: "BTC",
+		  url_image: "https://image.flaticon.com/icons/png/512/3342/3342766.png"
+		},
 
-Coin.create!(
-	description: "Ethereum",
-	acronym: "ETH",
-	url_image: "https://th.bing.com/th/id/R.e2a12cefdfb666cacd6fde11f6a19104?rik=SeNdhJbSfPQsZw&pid=ImgRaw"
-)
+		{
+		  description: "Ethereum",
+		  acronym: "ETH",
+		  url_image: "https://th.bing.com/th/id/R.e2a12cefdfb666cacd6fde11f6a19104?rik=SeNdhJbSfPQsZw&pid=ImgRaw"
+		},
 
-Coin.create!(
-	description: "Dash",
-	acronym: "DASH",
-	url_image: "https://i.pinimg.com/originals/99/81/52/99815216c69f59d6bb1322de48a38aae.png"
-)
+		{
+		   description: "Dash",
+		   acronym: "DASH",
+		   url_image: "https://i.pinimg.com/originals/99/81/52/99815216c69f59d6bb1322de48a38aae.png"
+		},
 
-Coin.create!(
-	description: "DogeCoin",
-	acronym: "DGC",
-	url_image: "https://th.bing.com/th/id/OIP.Q2nZOE1TrgBfHantVQQiqwHaHa?pid=ImgDet&rs=1"
-)
+		{
+			description: "DogeCoin",
+			acronym: "DGC",
+			url_image: "https://th.bing.com/th/id/OIP.Q2nZOE1TrgBfHantVQQiqwHaHa?pid=ImgDet&rs=1"
+	}
+	]
 
-puts "Moedas Cadastradas com Sucesso!!"
+ coins.each do |coin|
+	Coin.find_or_create_by!(coin)
+ end
+
+spinner.success("(Concluído)")
