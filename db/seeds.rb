d7f2e163c0cffd2969b36411f3d2827eaa6eb45f
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2021-07-29 18:56:57 +0000

    Task db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando Moedas..."
+
+Coin.create!(
+	description: "Bitcoin",
+	acronym: "BTC",
+	url_image: "https://image.flaticon.com/icons/png/512/3342/3342766.png"
+)
+
+Coin.create!(
+	description: "Ethereum",
+	acronym: "ETH",
+	url_image: "https://th.bing.com/th/id/R.e2a12cefdfb666cacd6fde11f6a19104?rik=SeNdhJbSfPQsZw&pid=ImgRaw"
+)
+
+Coin.create!(
+	description: "Dash",
+	acronym: "DASH",
+	url_image: "https://i.pinimg.com/originals/99/81/52/99815216c69f59d6bb1322de48a38aae.png"
+)
+
+Coin.create!(
+	description: "DogeCoin",
+	acronym: "DGC",
+	url_image: "https://th.bing.com/th/id/OIP.Q2nZOE1TrgBfHantVQQiqwHaHa?pid=ImgDet&rs=1"
+)
+
+puts "Moedas Cadastradas com Sucesso!!"
