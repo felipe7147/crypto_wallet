87bd4a4105a02eddc9c61157bae5d00a22b93b04
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2021-07-30 13:34:05 +0000

    Ativando o I18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+	def locale(locale)
+		if I18n.locale == :en
+			"Estados Unidos"
+		else
+			"Portugues do Brasil"
+		end
+	end
+
+
 	def data_br(data_us)
 		data_us.strftime("%d/%m/%Y")
 	end
