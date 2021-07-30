c2dcbfdf9badd35d36172bad1432b4ef18de7c10
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2021-07-30 10:38:26 +0000

    Corrigindo o CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_094848) do
+ActiveRecord::Schema.define(version: 2021_07_30_103443) do
 
   create_table "coins", force: :cascade do |t|
     t.string "description"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_07_30_094848) do
   end
 
   create_table "mining_types", force: :cascade do |t|
-    t.string "name"
+    t.string "description"
     t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
