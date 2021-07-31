c059299f3496860798dac078e81463095ba82aa6
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2021-07-31 11:18:22 +0000

    Cookies e Sessions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails - Felipe Almeida [COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails - Felipe Almeida [SESSION]"
     @meu_nome = params[:nome]
     @curso = params[:curso]
   end
